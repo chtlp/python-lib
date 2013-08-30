12d73d02c90661556c5446fdeedece9b0163c10b
Linpeng Tang <chnttlp@gmail.com>
2013-08-29 20:10:24 -0700

    new tools

--- py_tools.py
+++ py_tools.py
@@ -1,10 +1,13 @@
 import os
+import bisect
+import random
+import logging
 
-'''
-matplotlib fall back on 'agg' when DISPLAY is not set
-** not reliable
-'''
 def fall_back_agg():
+    """
+    matplotlib fall back on 'agg' when DISPLAY is not set
+    ** not reliable
+    """
     import matplotlib
     print os.getenv('DISPLAY')
     if not os.getenv('DISPLAY'):
@@ -13,3 +16,27 @@ def fall_back_agg():
 def use_agg():
     import matplotlib
     matplotlib.use('agg')
+
+def logspace2(start, stop, num=50, endpoint=True):
+    """
+    logspace points between [start, stop]
+    """
+    from numpy import logspace, log10
+    return logspace(log10(start), log10(stop), num, endpoint)
+
+def logging_debug():
+    return logging.getLogger().getEffectiveLevel() <= logging.DEBUG
+
+def average(l):
+    return sum(l) / float(len(l))
+
+def WeightedSelectionWithReplacement(l, n):
+  """Selects with replacement n random elements from a list of (item, weight) tuples."""
+  cum_weight = []
+  items = []
+  total_weight = 0.0
+  for item, weight in l:
+    total_weight += weight
+    cum_weight.append(total_weight)
+    items.append(item)
+  return [items[bisect.bisect(cum_weight, random.random()*total_weight)] for x in range(n)]
--- queue_tools.py
+++ queue_tools.py
@@ -5,11 +5,12 @@ import os
 import redis
 
 class TaskQueue(object):
-    def __init__(self, queue_name):
-        r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    def __init__(self, queue_name, host_name='localhost'):
+        r = redis.StrictRedis(host=host_name, port=6379, db=0)
         r.delete(queue_name)
         self.queue_name = queue_name
-        self.q = lightQueue(db=0, queue_name=queue_name)
+        self.host_name = host_name
+        self.q = lightQueue(host=host_name, db=0, queue_name=queue_name)
         self.py_queue = Queue.Queue()
 
     def add_task(self, func, *args, **kwargs):
@@ -27,5 +28,7 @@ class TaskQueue(object):
                     apply(task[0], task[1], task[2])
         else:
             os.system(('lightqueue start -e parallel -workers {workers} '
-                       '-qname {qname} -t 1').
-                      format(qname=self.queue_name, **locals()))
+                       '-qname {qname} -host {host_name} -t 1').
+                      format(qname=self.queue_name,
+                             host_name=self.host_name,
+                             **locals()))
